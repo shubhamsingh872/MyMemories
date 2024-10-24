c81029c1c5ed473b0620c6b6ed987ea6a3b7b8a5
Shubham Kumar <kshubhamsingh96311@gmail.com>
2024-10-24 15:13:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => (
     <Container maxWidth="lg">
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Home} /> 
         <Route path="/auth" exact component={Auth} />
       </Switch>
     </Container>
